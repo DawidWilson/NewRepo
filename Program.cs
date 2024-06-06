8dbfc5bfe65182c24f96c675892d4bd49f7e6da4
DawidWilson <86928107+DawidWilson@users.noreply.github.com>
2024-06-06 19:51:54 +0200

    ывапвырва

--- Program.cs
+++ Program.cs
@@ -1,1 +1,6 @@
-﻿
+﻿Console.Write("Введите ваше имя: ");
+string name = Console.ReadLine();
+Console.WriteLine("Меня зовут," + name);
+Console.Write("Введите ваш возраст: ");
+string age = Console.ReadLine();
+Console.WriteLine("Мой возраст, " + age);
